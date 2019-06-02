da7d0607d5193d1a4d50529b0f7302d51ff6f135
Ross Davis <ross.davis@targetx.com>
2019-06-02 12:44:18 -0700

    Add gocolly to help parse entries
    Signed-off-by: Ross Davis <ross.davis@targetx.com>

--- scrap_parse.go
+++ scrap_parse.go
@@ -1,26 +1,57 @@
 package main
 
 import (
+	// "database/sql"
 	"fmt"
+	// "github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
-	"io/ioutil"
-	"net/http"
+	// _ "github.com/mattn/go-sqlite3"
+	// "io/ioutil"
 )
 
-func main() {
-	resp, err := http.Get("https://sfbay.craigslist.org/search/apa?search_distance=3&postal=94118&max_price=2000&min_bathrooms=1&availabilityMode=0&sale_date=all+dates")
-	if err != nil {
-		fmt.Printf("Error")
-	}
-	defer resp.Body.Close()
+type ResultObj struct {
+	id        string
+	repost_id string
+	title     string
+	url       string
+	price     string
+}
 
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
-		if err2 != nil {
-			fmt.Printf("Error2")
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("craigslist.org", "sfbay.craigslist.org"),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("li.result-row", func(e *colly.HTMLElement) {
+		result_obj := ResultObj{
+			id:        e.Attr("data-pid"),
+			repost_id: e.Attr("data-repost-of"),
+			title:     e.ChildText("a.result-title"),
+			url:       e.ChildAttr("a.result-title", "href"),
 		}
-		bodyString := string(bodyBytes)
-		fmt.Printf(bodyString)
-	}
+		e.ForEach("a.result-image", func(_ int, ei *colly.HTMLElement) {
+			price := ei.ChildText("span.result-price")
+			result_obj.price = price
+		})
+		fmt.Printf("Post Id Found: %s\n", result_obj.id)
+		fmt.Printf("Post Repost Id Found: %s\n", result_obj.repost_id)
+		fmt.Printf("Post Title Found: %s\n", result_obj.title)
+		fmt.Printf("Post url: %s\n", result_obj.url)
+		fmt.Printf("Post price: %s\n", result_obj.price)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		// c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://sfbay.craigslist.org/search/apa?search_distance=3&postal=94118&max_price=2000&availabilityMode=0&sale_date=all+dates")
 
 }
