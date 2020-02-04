c0557f265148538ee0ac1816f8debdaab267a7af
Ross Davis <ross.davis@targetx.com>
2020-02-04 10:41:22 -0800

    Linting, error checking
    Signed-off-by: Ross Davis <ross.davis@targetx.com>

--- scrape_parse.go
+++ scrape_parse.go
@@ -12,30 +12,31 @@ import (
 	// "io/ioutil"
 )
 
+// ResultObj is a data structure for storing the results of the page scrape
 type ResultObj struct {
-	id          string
-	repost_id   string
-	title       string
-	url         string
-	price       string
-	housing     string
-	hood        string
-	posted_time time.Time
+	id         string
+	repostId   string
+	title      string
+	url        string
+	price      string
+	housing    string
+	hood       string
+	postedTime time.Time
 }
 
 func main() {
 	postal := flag.Int("postal", 94118, "Zip Code of search area")
 	distance := flag.Int("distance", 3, "Search radius away from zip code")
 	// min_price := flag.Int("min_price", 1000, "Min price to search for")
-	max_price := flag.Int("max_price", 2500, "Max price to search for")
+	maxPrice := flag.Int("max_price", 2500, "Max price to search for")
 
 	flag.Parse()
 
-	url_base := "https://sfbay.craigslist.org/search/nby/apa"
-	query_string := fmt.Sprintf("?search_distance=%d&postal=%d&max_price=%d&availabilityMode=0&sale_date=all+date", distance, postal, max_price)
+	urlBase := "https://sfbay.craigslist.org/search/nby/apa"
+	queryString := fmt.Sprintf("?search_distance=%d&postal=%d&max_price=%d&availabilityMode=0&sale_date=all+date", distance, postal, maxPrice)
 	database, _ := sql.Open("sqlite3", "./db/craigslist.db")
 	statement, _ := database.Prepare(`CREATE TABLE IF NOT EXISTS results 
-					(id INTEGER PRIMARY KEY, repost_id INTEGER, 
+					(id INTEGER PRIMARY KEY, repostId INTEGER, 
 					title TEXT, url TEXT, price TEXT, housing TEXT, 
 					hood TEXT, posted_time DATETIME,
 					mod_time DATETIME DEFAULT CURRENT_TIMESTAMP)`)
@@ -50,37 +51,37 @@ func main() {
 	// On every a element which has href attribute call callback
 	c.OnHTML("li.result-row", func(e *colly.HTMLElement) {
 		fmt.Printf("Time Stamp: ")
-		posted_time := e.ChildAttr("time.result-date", "datetime")
-		fmt.Printf(posted_time + "\n")
+		postedTime := e.ChildAttr("time.result-date", "datetime")
+		fmt.Printf(postedTime + "\n")
 		shortForm := "2006-01-02 15:04"
 		loc, _ := time.LoadLocation("America/Los_Angeles")
-		post_time, _ := time.ParseInLocation(shortForm, posted_time, loc)
+		postTime, _ := time.ParseInLocation(shortForm, postedTime, loc)
 		fmt.Printf("Posted Time: ")
-		fmt.Println(post_time.In(loc))
-		result_obj := ResultObj{
-			id:          e.Attr("data-pid"),
-			repost_id:   e.Attr("data-repost-of"),
-			title:       e.ChildText("a.result-title"),
-			url:         e.ChildAttr("a.result-title", "href"),
-			posted_time: post_time,
+		fmt.Println(postTime.In(loc))
+		resultObj := ResultObj{
+			id:         e.Attr("data-pid"),
+			repostId:   e.Attr("data-repost-of"),
+			title:      e.ChildText("a.result-title"),
+			url:        e.ChildAttr("a.result-title", "href"),
+			postedTime: postTime,
 		}
 		e.ForEach("span.result-meta", func(_ int, ei *colly.HTMLElement) {
 			price := ei.ChildText("span.result-price")
 			housing := ei.ChildText("span.housing")
 			hood := ei.ChildText("span.result-hood")
-			result_obj.price = price
-			result_obj.housing = housing
-			result_obj.hood = hood
+			resultObj.price = price
+			resultObj.housing = housing
+			resultObj.hood = hood
 		})
-		row := database.QueryRow(fmt.Sprintf("SELECT id FROM results where id='%s'", result_obj.id))
+		row := database.QueryRow(fmt.Sprintf("SELECT id FROM results where id='%s'", resultObj.id))
 		var id string
 		row.Scan(&id)
-		if (id != result_obj.id) && (id != result_obj.repost_id) {
+		if (id != resultObj.id) && (id != resultObj.repostId) {
 			fmt.Printf("New Record Insert!!!!!!!!\n")
-			statement, _ := database.Prepare(`INSERT INTO results (id, repost_id, title, url, price, posted_time, housing, hood)
+			statement, _ := database.Prepare(`INSERT INTO results (id, repostId, title, url, price, postedTime, housing, hood)
 			   VALUES (?, ?, ?, ? ,?, ?, ?, ?)`)
-			statement.Exec(result_obj.id, result_obj.repost_id, result_obj.title, result_obj.url,
-				result_obj.price, result_obj.posted_time, result_obj.housing, result_obj.hood)
+			statement.Exec(resultObj.id, resultObj.repostId, resultObj.title, resultObj.url,
+				resultObj.price, resultObj.postedTime, resultObj.housing, resultObj.hood)
 		}
 		rows, _ := database.Query("SELECT id, title, price FROM results")
 		var title string
@@ -97,6 +98,6 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit(url_base + query_string)
+	c.Visit(urlBase + queryString)
 
 }
